f9e469ad6b2d081997efbd540402021a10ebdf79
Asawer Naseer <muhammadasawer75@gmail.com>
2025-04-12 15:18:14 +0500

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,7 +7,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (!Array.isArray(blogs) || blogs.length === 0) {
+    return null;
+  }
+
+  return blogs.reduce((favorite, current) => {
+    return current.likes > favorite.likes ? current : favorite;
+  });
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
